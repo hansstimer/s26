2ac68bd2326719125f14842b354a1f85f1d50ec3
Martin Hess <marty@twitch.tv>
2014-07-06 16:52:13 -0700

    Basic udp write/read test

--- s26_test.go
+++ s26_test.go
@@ -2,8 +2,8 @@ package s26
 
 import (
 	"net"
-	"runtime"
 	"testing"
+	"time"
 )
 
 func TestS26(t *testing.T) {
@@ -11,63 +11,54 @@ func TestS26(t *testing.T) {
 }
 
 func TestWriteToUDP(t *testing.T) {
-	switch runtime.GOOS {
-	case "plan9":
-		t.Skipf("skipping test on %q", runtime.GOOS)
+	listenAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("Resolve failed: %v", err)
 	}
 
-	l, err := net.ListenPacket("udp", "127.0.0.1:0")
+	l, err := net.ListenUDP("udp", listenAddr)
 	if err != nil {
 		t.Fatalf("Listen failed: %v", err)
 	}
 	defer l.Close()
 
-	testWriteToConn(t, l.LocalAddr().String())
-	testWriteToPacketConn(t, l.LocalAddr().String())
-}
+	message := "UDP test message"
 
-func testWriteToConn(t *testing.T, raddr string) {
-	udpAddr, err := net.ResolveUDPAddr("udp", raddr)
-	if err != nil {
-		t.Fatalf("Resolve failed: %v", err)
+	done := make(chan bool, 1)
+	go func() {
+		var b [576]byte
+		i, err := l.Read(b[:])
+		if err != nil {
+			t.Fatalf("Read err: %v", err)
+		}
+		if string(b[:i]) != message {
+			t.Fatalf("Expected: %v, got: %v\n", message, string(b[:i]))
+		}
+		done <- true
+	}()
+
+	go func(raddr string) {
+		udpAddr, err := net.ResolveUDPAddr("udp", raddr)
+		if err != nil {
+			t.Fatalf("Resolve failed: %v", err)
+		}
+
+		c, err := net.DialUDP("udp", nil, udpAddr)
+		if err != nil {
+			t.Fatalf("Dial failed: %v", err)
+		}
+		defer c.Close()
+
+		_, err = c.Write([]byte(message))
+		if err != nil {
+			t.Fatalf("Write failed: %v", err)
+		}
+	}(l.LocalAddr().String())
+
+	select {
+	case <-done:
+	case <-time.After(2 * time.Second):
+		t.Fatal("timeout waiting for UCP read")
 	}
 
-	c, err := net.DialUDP("udp", nil, udpAddr)
-	if err != nil {
-		t.Fatalf("Dial failed: %v", err)
-	}
-	defer c.Close()
-
-	_, err = c.Write([]byte("Connection-oriented mode socket"))
-	if err != nil {
-		t.Fatalf("Write failed: %v", err)
-	}
-}
-
-func testWriteToPacketConn(t *testing.T, raddr string) {
-	c, err := net.ListenPacket("udp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatalf("ListenPacket failed: %v", err)
-	}
-	defer c.Close()
-
-	ra, err := net.ResolveUDPAddr("udp", raddr)
-	if err != nil {
-		t.Fatalf("ResolveUDPAddr failed: %v", err)
-	}
-
-	_, err = c.(*net.UDPConn).WriteToUDP([]byte("Connection-less mode socket"), ra)
-	if err != nil {
-		t.Fatalf("WriteToUDP failed: %v", err)
-	}
-
-	_, err = c.WriteTo([]byte("Connection-less mode socket"), ra)
-	if err != nil {
-		t.Fatalf("WriteTo failed: %v", err)
-	}
-
-	_, err = c.(*net.UDPConn).Write([]byte("Connection-less mode socket"))
-	if err == nil {
-		t.Fatal("Write should fail")
-	}
 }
